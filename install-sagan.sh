53e778f005e32b653684f3156ae25c44ca48fb0c
sbragagnolo <santiagobragagnolo@gmail.com>
2023-01-04 18:23:59 +0100

    makign work sagan install scirpt

--- install-sagan.sh
+++ install-sagan.sh
@@ -2,7 +2,8 @@
 
 mkdir bin
 cd bin
-wget -O- get.pharo.org/100+vm | bash
+wget -O- get.pharo.org/100 | bash
+wget -O- get.pharo.org/vm100 | bash
 ./pharo Pharo.image eval --save "Metacello new
 		repository: 'github://sbragagnolo/Sagan:main/src';
 		baseline: 'Sagan';
